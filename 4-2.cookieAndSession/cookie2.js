6990267fd713ffbca5ae1be1d3b0c03d5f567752
fireworks80 <fireworks80@naver.com>
2021-11-07 22:27:44 +0900

    cookie

--- 4-2.cookieAndSession/cookie2.js
+++ 4-2.cookieAndSession/cookie2.js
@@ -3,8 +3,7 @@ const fs = require('fs').promises;
 const url = require('url');
 const qs = require('querystring');
 
-const parseCookies = (cookie = '') => {
-  console.log(cookie);
+const parseCookie = (cookie = '') => {
   return cookie
     .split(';')
     .map((v) => v.split('='))
@@ -16,37 +15,41 @@ const parseCookies = (cookie = '') => {
 
 http
   .createServer(async (req, res) => {
-    // console.log(req.headers.cookie);
-    const cookies = parseCookies(req.headers.cookie);
-    // console.log(cookies);
+    const cookies = parseCookie(req.headers.cookie);
+
     if (req.url.startsWith('/login')) {
       const { query } = url.parse(req.url);
       const { name } = qs.parse(query);
       const expires = new Date();
 
-      expires.setMinutes(expires.getMinutes + 5);
+      expires.setMinutes(expires.getMinutes() + 5);
+
       res.writeHead(302, {
         Location: '/',
-        'Set-Cookie': `name=${decodeURIComponent(name)}; Expires=${expires.toGMTString()}; Httponly; Path=/`,
+        'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toUTCString()}; HttpOnly; Path=/`,
       });
-      return res.end();
-    } else if (cookies && cookies.name) {
-      console.log('has');
-      res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-      res.end(`${cookies.name}님 안녕하세요`);
-    } else {
+      res.end();
+    } // login
+
+    // cookie가 있으면
+    else if (cookies.name) {
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      res.end(`<h1>안녕하세요 ${cookies.name}</h1>`);
+    }
+
+    // 처음 들어 왔다면
+    else {
+      console.log('else');
       try {
         const data = await fs.readFile('./cookie.html');
-
         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
         res.end(data);
-      } catch (err) {
-        console.error(err);
+      } catch (error) {
         res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
-        res.end(err.message);
+        res.end(error);
       }
-    } // if / else
+    }
   })
   .listen(8080, () => {
-    console.log('8080 listen');
+    console.log('8080 port');
   });
