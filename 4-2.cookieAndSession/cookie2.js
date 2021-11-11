d9f53608f5e97ef48c63348a5e75447ea266bd4d
fireworks80 <fireworks80@naver.com>
2021-11-11 23:17:22 +0900

    cookie 다시 작성, nodemon 추가

--- 4-2.cookieAndSession/cookie2.js
+++ 4-2.cookieAndSession/cookie2.js
@@ -3,12 +3,12 @@ const fs = require('fs').promises;
 const url = require('url');
 const qs = require('querystring');
 
-const parseCookie = (cookie = '') => {
-  return cookie
+const parseCookie = (cookies = '') => {
+  return cookies
     .split(';')
-    .map((v) => v.split('='))
+    .map((cookie) => cookie.split('='))
     .reduce((acc, [k, v]) => {
-      acc[k.trim()] = decodeURIComponent(v);
+      acc[k] = decodeURIComponent(v);
       return acc;
     }, {});
 };
@@ -20,33 +20,25 @@ http
     if (req.url.startsWith('/login')) {
       const { query } = url.parse(req.url);
       const { name } = qs.parse(query);
-      const expires = new Date();
-
-      expires.setMinutes(expires.getMinutes() + 5);
+      const date = new Date();
+      date.setMinutes(date.getMinutes() + 5);
 
       res.writeHead(302, {
         Location: '/',
-        'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toUTCString()}; HttpOnly; Path=/`,
+        'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${date.toUTCString()}; HttpOnly; Path=/`,
       });
       res.end();
-    } // login
-
-    // cookie가 있으면
-    else if (cookies.name) {
+    } else if (cookies && cookies.name) {
       res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-      res.end(`<h1>안녕하세요 ${cookies.name}</h1>`);
-    }
-
-    // 처음 들어 왔다면
-    else {
-      console.log('else');
+      res.end(`hello ${cookies.name}`);
+    } else {
       try {
         const data = await fs.readFile('./cookie.html');
         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
         res.end(data);
-      } catch (error) {
+      } catch (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
-        res.end(error);
+        res.end(err);
       }
     }
   })
